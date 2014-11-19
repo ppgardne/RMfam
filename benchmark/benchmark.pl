0b2a84405e60c741c0a6b257efdbdbeb325cbf35
ppgardne <ppgardner@gmail.com>
2014-11-20 11:36:59 +1300

    Long overdue commit....

--- benchmark/benchmark.pl
+++ benchmark/benchmark.pl
@@ -5,6 +5,11 @@ use strict;
 
 #grep -v ^"#" true_positives.txt | perl -lane 'if($F[1] eq "k-turn"){print "$F[0]\tk-turn-1\t$F[2]\n$F[0]\tk-turn-2\t$F[2]"}elsif($F[1] eq "sarcin-ricin"){print "$F[0]\tsarcin-ricin-1\t$F[2]\n$F[0]\tsarcin-ricin-2\t$F[2]"}elsif($F[1] eq "Terminator"){print "$F[0]\tterminator1\t$F[2]\n$F[0]\tterminator2\t$F[2]"}else{$F=join("\t", @F); print $F}' | sort -d > true_positives_processed.txt
 
+my $totalTrues=0;
+my $totalFalse=2208*10; #number of shuffled alignments
+my %totalTruesB2;
+my %rmfam;
+my %seenMatch;
 my $trueFile = "true_positives.txt"; 
 open(TF, "< $trueFile"); 
 my (%fam2mots, %fammotcat);
@@ -14,10 +19,38 @@ while(my $t=<TF>){
     if(scalar(@t)>1){
 	push(@{ $fam2mots{$t[0]} },$t[1]);
 	$fammotcat{"$t[0]:$t[1]"}=1;
+	$seenMatch{"$t[0]:$t[1]"}=0;
+	$totalTrues++;
+	my $rmfam = $t[1];
+	if (not defined($totalTruesB2{$rmfam})){
+	    $totalTruesB2{$rmfam}=0; 
+	}
+	$totalTruesB2{$rmfam}++;
+	$rmfam{$rmfam}=1;
+
     }
     
 }
 close(TF);
+
+my $cmFile = "../scripts/RMfam.cm"; 
+open(CF, "< $cmFile"); 
+open(UT, "> thresholds.dat"); 
+print UT "NAME\tGA\n";
+my ($motName, $motGA, %motifs);
+while(my $cf=<CF>){
+    if ($cf=~/^NAME\s+(\S+)/){
+	$motName=$1; 
+    }
+    elsif($cf=~/^GA\s+(\S+)/){
+	$motGA=$1;
+	print UT "$motName\t$motGA\n";
+	$motifs{$motName}=$motGA;
+    }
+}
+close(CF);
+close(UT);
+
 
 ######################################################################
 #ALIGNMENTS
@@ -25,37 +58,53 @@ close(TF);
 my (@mappings);
 #my $netFile  = "network.dat";
 my $netFile  = "../annotations/Rfam.tab";
-my $totalTrues=0;
 #array of arrays
-open(NF, "< $netFile"); 
+open(NF, "sort -k5nr < $netFile |"); 
+open(UTT, "> true-predictions-alignments.dat"); 
+open(UTF, "> false-predictions-alignments.dat"); 
+$totalTrues=0; 
 while(my $n=<NF>){
     
     my @n=split(/\s+/, $n);
     if($n=~/RF\d{5}\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)/){
 	my ($rfam,$rmfam,$n1,$n2,$n3)=($1,$2,$3,$4,$5);
 	if( defined($fammotcat{"$rfam:$rmfam"}) ){
+	    $seenMatch{"$rfam:$rmfam"}++;
 	    #TRUE!
 	    push(@mappings, [ $rfam,$rmfam,$n1,$n2,$n3, "true" ]); 
 	    $totalTrues++;
-	    #print "TRUE: $n";
+	    $rmfam{$rmfam}=1;
+	    print UTT "$n";
 	}
 	else{
-	    print "FALSE: $n";
+	    print UTF "$n";
+	    #print "FALSE: $n";
 	}
     }
 }
 close(NF); 
+close(UTT); 
+close(UTF); 
 
-#exit(0);
+open(UT, "> false-negative-alignments.dat");
+foreach my $k ( sort { $a cmp $b } keys %seenMatch){
+    my @k = split(/:/, $k);
+    print UT "$k[0]\t$k[1]\n" if ($seenMatch{$k} == 0);
+}
+close(UT);
 
 my $netSFile = "../annotations/Rfam-shuffle.tab";
-my $totalFalses=0;
+$totalFalse=0;
+my %totalFalseB2;
 open(NF, "< $netSFile"); 
 while(my $n=<NF>){
     
     my @n=split(/\s+/, $n);
     push(@mappings, [ @n[1..5], "false" ]); 
-    $totalFalses++;
+    $totalFalse++;
+    
+    $totalFalseB2{$n[2]}=0 if (not defined($totalFalseB2{$n[2]})); 
+    $totalFalseB2{$n[2]}++;
     
 }
 close(NF); 
@@ -63,134 +112,186 @@ close(NF);
 
 #TP, TN, FP, FN
 
+my (%tpB2,%tnB2,%fpB2,%fnB2); 
+foreach my $rm (keys %rmfam){
+    $totalFalseB2{$rm}=0 if (not defined($totalFalseB2{$rm}));
+    $totalTruesB2{$rm} =0 if (not defined($totalTruesB2{$rm}));
+    ($tpB2{$rm},$tnB2{$rm},$fpB2{$rm},$fnB2{$rm})=(0,2208*10,0,$totalTruesB2{$rm}); #$totalFalseB2{$rm}
+    #print "motif:[$rm] tF[$totalFalseB2{$rm}] tT[$totalTruesB2{$rm}]\n"; 
+}
+
 #sorted on sum-bits
 my @labels=("fracSeqs", "sumBits", "weightedSumBits"); 
+open(UT2, "> ROC_data_sumBits_benchmark2\.dat");
+print UT2 "bits\tTP\tTN\tFP\tFN\tMCC\tSENS\tSPEC\tPPV\tNPV\tACC\tFDR\tmotif\n";
+my(%mccB2, %sensB2, %specB2, %ppvB2, %npvB2, %accB2, %fdrB2);
 for(my $i=0; $i<scalar(@labels); $i++){
     my $arrayCoord=$i+2; 
     @mappings = sort { $b->[$arrayCoord] <=> $a->[$arrayCoord] } @mappings; 
-    my ($tp,$tn,$fp,$fn)=(0,$totalFalses,0,$totalTrues); 
+    my ($tp,$tn,$fp,$fn)=(0,$totalFalse,0,$totalTrues); #$totalFalse
     
     open(UT, "> ROC_data_$labels[$i]\.dat");
     
     print UT "$labels[$i]\tTP\tTN\tFP\tFN\tMCC\tSENS\tSPEC\tPPV\tNPV\tACC\tFDR\n";
     foreach my $row (@mappings){
 	
+	my $rmfam=$row->[1];
 	my $str=join("][", @{$row});
 	if($row->[5] eq "true"){
 	    $tp++;
 	    $fn--;
+	    if($labels[$i] eq "sumBits"){
+		$tpB2{$rmfam}++;
+		$fnB2{$rmfam}--;	    
+	    }
 	}
 	else{
 	    $fp++; 
 	    $tn--;
+	    if($labels[$i] eq "sumBits"){
+		$fpB2{$rmfam}++;
+		$tnB2{$rmfam}--;
+	    }
 	}
 	
 	my($mcc, $sens, $spec, $ppv, $npv, $acc, $fdr)=acc($tp,$tn,$fp,$fn); 
 	printf UT "$row->[$arrayCoord]\t$tp\t$tn\t$fp\t$fn\t%0.2f\t%0.2f\t%0.2f\t%0.2f\t%0.2f\t%0.2f\t%0.2f\n", $mcc, $sens, $spec, $ppv, $npv, $acc, $fdr; 
 	
+	if($labels[$i] eq "sumBits" && defined($tpB2{$rmfam}) && defined($tnB2{$rmfam}) && defined($fpB2{$rmfam}) && defined($fnB2{$rmfam}) ){
+	    ($mccB2{$rmfam}, $sensB2{$rmfam}, $specB2{$rmfam}, $ppvB2{$rmfam}, $npvB2{$rmfam}, $accB2{$rmfam}, $fdrB2{$rmfam})=acc($tpB2{$rmfam},$tnB2{$rmfam},$fpB2{$rmfam},$fnB2{$rmfam}); 
+	    printf UT2 "$row->[$arrayCoord]\t$tpB2{$rmfam}\t$tnB2{$rmfam}\t$fpB2{$rmfam}\t$fnB2{$rmfam}\t%0.2f\t%0.2f\t%0.2f\t%0.2f\t%0.2f\t%0.2f\t%0.2f\t$rmfam\n", $mccB2{$rmfam}, $sensB2{$rmfam}, $specB2{$rmfam}, $ppvB2{$rmfam}, $npvB2{$rmfam}, $accB2{$rmfam}, $fdrB2{$rmfam}; 
+	}
     }
     
     close(UT); 
 }
+close(UT2); 
 
 ######################################################################
 #SEQUENCES
 @mappings=();
 $totalTrues=0;
-foreach my $f (keys %fam2mots){
-    my $file="../data/sequence-annotations/rfam/" . $f  . "-seed.stk.selectn5.network";
-    
-    if(-e $file){
-	open(IN, "< $file"); 
-	while(my $in=<IN>){
-	    chomp($in); 
-	    my @in=split(/\t/, $in);
-	    if( defined($fammotcat{"$f:$in[1]"}) ){
-		#TRUE!
-		push(@mappings, [ $in[2], "true" ]); 
-		$totalTrues++;
-	    }	    
+my $file="../annotations/Rfam-sequences.tab";
+my %totalTruesSeq;
+if(-e $file){
+    open(IN, "< $file"); 
+    while(my $in=<IN>){
+	chomp($in); 
+	my @in=split(/\t/, $in);
+	if( defined($fammotcat{"$in[0]:$in[1]"}) ){
+	    #TRUE!
+	    push(@mappings, [ $in[2], "true", $in[0], $in[1] ]); 
+	    $totalTrues++;
+	    
+	    $totalTruesSeq{$in[1]}=0 if (not defined($totalTruesSeq{$in[1]}));
+	    $totalTruesSeq{$in[1]}++;
 	}
-	close(IN); 
     }
+    close(IN); 
 }
 
-$totalFalses=0;
-my @files=glob("../data/sequence-annotations/rfam-shuffle/*-shuffle-selectn5.fa.network"); 
-foreach my $file (@files){
-    if(-e $file){
-	open(IN, "< $file"); 
-	while(my $in=<IN>){
-	    chomp($in); 
-	    my @in=split(/\t/, $in);
-	    
-	    if( defined($in[1]) && $file=~/rfam-shuffle\/(\S+)-selectn5.fa.network/){
-		#FALSE!
-		push(@mappings, [ $in[2], "false" ]); 
-		$totalFalses++;
-	    }
-	    
+my %totalFalseSeq;
+$totalFalse=0;
+$file="../annotations/Rfam-shuffle-sequences.tab"; 
+if(-e $file){
+    open(IN, "< $file"); 
+    while(my $in=<IN>){
+	chomp($in); 
+	my @in=split(/\t/, $in);
+	
+	if( scalar(@in) == 3 && $in[2] =~ /^\d+\.\d+$/){
+	    #DAS IST FALSCH!
+	    push(@mappings, [ $in[2], "false", $in[0], $in[1] ]); 
+	    $totalFalse++;
+	    $totalFalseSeq{$in[1]}=0 if (not defined($totalFalseSeq{$in[1]}));
+	    $totalFalseSeq{$in[1]}++;
 	}
-	close(IN); 
+	
     }
+    close(IN); 
 }
 
 
-my ($tp,$tn,$fp,$fn)=(0,$totalFalses,0,$totalTrues); 
+my (%tpSeq,%tnSeq,%fpSeq,%fnSeq); 
+foreach my $rm (keys %motifs){
+    $totalFalseSeq{$rm}=0 if (not defined($totalFalseSeq{$rm}));
+    $totalTruesSeq{$rm}=0 if (not defined($totalTruesSeq{$rm}));
+    ($tpSeq{$rm},$tnSeq{$rm},$fpSeq{$rm},$fnSeq{$rm})=(0,$totalFalseSeq{$rm},0,$totalTruesSeq{$rm}); 
+    print "$rm:[$tpSeq{$rm},$tnSeq{$rm},$fpSeq{$rm},$fnSeq{$rm}]\n";
+}
+
+my ($tp,$tn,$fp,$fn)=(0,$totalFalse,0,$totalTrues); 
 @mappings = sort { $b->[0] <=> $a->[0] } @mappings; 
 
-open(UT, "> ROC_data_single_seq_sumBits\.dat");
+open(UT,  "> ROC_data_single_seq_sumBits.dat");
+open(UTS, "> ROC_data_single_seq_sumBits-motifPerMotif.dat");
 
-print UT "sngl_seq_sumBits\tTP\tTN\tFP\tFN\tMCC\tSENS\tSPEC\tPPV\tNPV\tACC\tFDR\n";
+print UT  "sngl_seq_sumBits\tTP\tTN\tFP\tFN\tMCC\tSENS\tSPEC\tPPV\tNPV\tACC\tFDR\n";
+print UTS "sngl_seq_sumBits\tTP\tTN\tFP\tFN\tMCC\tSENS\tSPEC\tPPV\tNPV\tACC\tFDR\tmotif\n";
 foreach my $row (@mappings){
     
-    my $str=join("][", @{$row});
+    my $rmfam=$row->[3];
     if($row->[1] eq "true"){
 	$tp++;
 	$fn--;
+	$tpSeq{$rmfam}++;
+	$fnSeq{$rmfam}--;
     }
     else{
 	$fp++; 
 	$tn--;
+	$fpSeq{$rmfam}++;
+	$tnSeq{$rmfam}--;
     }
     
-    my($mcc, $sens, $spec, $ppv, $npv, $acc, $fdr)=acc($tp,$tn,$fp,$fn); 
+    my ($mcc, $sens, $spec, $ppv, $npv, $acc, $fdr)=acc($tp,$tn,$fp,$fn); 
     printf UT "$row->[0]\t$tp\t$tn\t$fp\t$fn\t%0.2f\t%0.2f\t%0.2f\t%0.2f\t%0.2f\t%0.2f\t%0.2f\n", $mcc, $sens, $spec, $ppv, $npv, $acc, $fdr; 
+    
+    ($mcc, $sens, $spec, $ppv, $npv, $acc, $fdr)=acc($tpSeq{$rmfam},$tnSeq{$rmfam},$fpSeq{$rmfam},$fnSeq{$rmfam}); 
+    printf UTS "$row->[0]\t$tpSeq{$rmfam}\t$tnSeq{$rmfam}\t$fpSeq{$rmfam}\t$fnSeq{$rmfam}\t%0.2f\t%0.2f\t%0.2f\t%0.2f\t%0.2f\t%0.2f\t%0.2f\t$rmfam\n", $mcc, $sens, $spec, $ppv, $npv, $acc, $fdr; 
     
 }
 close(UT); 
+close(UTS); 
 
 ######################################################################
-#Benchmark 2:
+#Benchmark 2: benchmarking on the training data: Rmfam seqs = true, shuffled negative control sequences
 #echo -e "db\tseq\tbits\teval\tmotif" > RMfam/benchmark/benchmark2.txt && cat */seq-scores.dat | egrep '^SEED|^PDB-shuffle' | sort -k3nr >> RMfam/benchmark/benchmark2.txt
 
 open(B2, "< benchmark2.txt");
 my @b2=<B2>;
 close(B2);
-#my ($tp,$tn,$fp,$fn)=(0,$totalFalses,0,$totalTrues); 
-my (%totalFalses, %totalTrues);
+#my ($tp,$tn,$fp,$fn)=(0,$totalFalse,0,$totalTrues); 
+my (%totalFalse, %totalTrues);
+($totalFalse,$totalTrues)=(0,0);
 foreach my $b2 (@b2){
     next if $b2 =~ /^db/; 
     chomp($b2);
     
     my @b=split(/\t/, $b2);
-    ($totalFalses{$b[4]}, $totalTrues{$b[4]})=(0,0) if (not defined($totalTrues{$b[4]})); 
+    ($totalFalse{$b[4]}, $totalTrues{$b[4]})=(0,0) if (not defined($totalTrues{$b[4]})); 
     
-    if($b2=~/^SEED-shuffled/){
-	$totalFalses{$b[4]}++;	
+    if($b2=~/^SEED-shuffled/ or $b2=~/^PDB-shuffle/){
+	$totalFalse{$b[4]}++;	
+	$totalFalse++;
     }
     elsif($b2=~/^SEED\t/){
 	$totalTrues{$b[4]}++;
+	$totalTrues++;
     }
 }
 
+($tp,$tn,$fp,$fn)=(0,$totalFalse,0,$totalTrues); 
 my (%tp,%tn,%fp,%fn);
 foreach my $k (keys %totalTrues){
-    ($tp{$k},$tn{$k},$fp{$k},$fn{$k})=(0,$totalFalses{$k},0,$totalTrues{$k}); 
+    ($tp{$k},$tn{$k},$fp{$k},$fn{$k})=(0,$totalFalse{$k},0,$totalTrues{$k}); 
 }
 
-open(UT, "> ROC_benchmark2\.dat");
-print UT "bits\tTP\tTN\tFP\tFN\tMCC\tSENS\tSPEC\tPPV\tNPV\tACC\tFDR\tmotif\n";
+
+open(UT,  "> ROC_benchmark2.dat");
+open(UTA, "> ROC_benchmark2-all.dat");
+print UT  "bits\tTP\tTN\tFP\tFN\tMCC\tSENS\tSPEC\tPPV\tNPV\tACC\tFDR\tmotif\n";
+print UTA "bits\tTP\tTN\tFP\tFN\tMCC\tSENS\tSPEC\tPPV\tNPV\tACC\tFDR\n";
 foreach my $row (@b2){
     chomp($row);
     my @row = split(/\t/, $row); 
@@ -199,17 +300,26 @@ foreach my $row (@b2){
     if($row[0] eq "SEED"){
 	$tp{$row[4]}++;
 	$fn{$row[4]}--;
+	$fn{$row[4]} = 0 if $fn{$row[4]}<0;
+	$tp++;
+	$fn--;
     }
-    else{
+    elsif($row[0] eq "SEED-shuffled" or $row[0] eq "PDB-shuffle"){
 	$fp{$row[4]}++; 
 	$tn{$row[4]}--;
+	$tn{$row[4]} = 0 if $tn{$row[4]}<0;
+	$fp++;
+	$tn--;
     }
     
     my($mcc, $sens, $spec, $ppv, $npv, $acc, $fdr)=acc($tp{$row[4]},$tn{$row[4]},$fp{$row[4]},$fn{$row[4]}); 
-    printf UT "$row[2]\t$tp{$row[4]}\t$tn{$row[4]}\t$fp{$row[4]}\t$fn{$row[4]}\t%0.2f\t%0.2f\t%0.2f\t%0.2f\t%0.2f\t%0.2f\t%0.2f\t$row[4]\n", $mcc, $sens, $spec, $ppv, $npv, $acc, $fdr; 
-     
+    printf UT  "$row[2]\t$tp{$row[4]}\t$tn{$row[4]}\t$fp{$row[4]}\t$fn{$row[4]}\t%0.2f\t%0.2f\t%0.2f\t%0.2f\t%0.2f\t%0.2f\t%0.2f\t$row[4]\n", $mcc, $sens, $spec, $ppv, $npv, $acc, $fdr; 
+    ($mcc, $sens, $spec, $ppv, $npv, $acc, $fdr)=acc($tp,$tn,$fp,$fn); 
+    printf UTA "$row[2]\t$tp\t$tn\t$fp\t$fn\t%0.2f\t%0.2f\t%0.2f\t%0.2f\t%0.2f\t%0.2f\t%0.2f\n", $mcc, $sens, $spec, $ppv, $npv, $acc, $fdr; 
+    
 }
 close(UT);
+close(UTA);
 
 ######################################################################
 #PLOT RESULTS
--- benchmark/plotROC.R
+++ benchmark/plotROC.R
@@ -1,9 +1,14 @@
 #R CMD BATCH --no-save plotROC.R
 
+######################################################################
+#benchmark1: RMfam
+
 rocFS      <-read.table("ROC_data_fracSeqs.dat",          header = T, sep = "\t")
 rocSB      <-read.table("ROC_data_sumBits.dat",           header = T, sep = "\t")
 rocWB      <-read.table("ROC_data_weightedSumBits.dat",   header = T, sep = "\t")
 rocSSSB    <-read.table("ROC_data_single_seq_sumBits.dat",header = T, sep = "\t")
+rocSSSBM   <-read.table("ROC_data_single_seq_sumBits-motifPerMotif.dat",header = T, sep = "\t")
+rocB2ALL   <-read.table("ROC_benchmark2-all.dat",           header = T, sep = "\t")
 
 #METRIC	TP	TN	FP	FN	MCC	SENS	SPEC	PPV	NPV	ACC	FDR
 #fracSeqs
@@ -12,7 +17,7 @@ rocSSSB    <-read.table("ROC_data_single_seq_sumBits.dat",header = T, sep = "\t"
 
 ########
 
-pdf(file="ROC_accuracy.pdf", width=20, height=20)
+pdf(file="figures/ROC_accuracy.pdf", width=20, height=20)
 op<-par(mfrow=c(2,2),cex=3.0)
 plot(rocFS$SPEC, rocFS$SENS, col="red", lwd=8, type="l",ylim=c(0,1),xlim=c(0,1),xlab="Specificity",ylab="Sensitivity",main="ROC plot")      
 lines(rocSB$SPEC, rocSB$SENS, col="blue",lwd=8)
@@ -22,7 +27,7 @@ points(rocFS$SPEC[rocFS$MCC==max(rocFS$MCC)], rocFS$SENS[rocFS$MCC==max(rocFS$MC
 points(rocSB$SPEC[rocSB$MCC==max(rocSB$MCC)], rocSB$SENS[rocSB$MCC==max(rocSB$MCC)], col="blue",pch="x",lwd=12)
 points(rocWB$SPEC[rocWB$MCC==max(rocWB$MCC)], rocWB$SENS[rocWB$MCC==max(rocWB$MCC)], col="purple",pch="x",lwd=12)
 points(rocSSSB$SPEC[rocSSSB$MCC==max(rocSSSB$MCC)], rocSSSB$SENS[rocSSSB$MCC==max(rocSSSB$MCC)], col="cyan",pch="x",lwd=12)
-#legend("bottomleft",c("fraction seqs.","sum bits", "weighted sum bits", "single-sequence sum bits"),col=c("red","blue","purple", "cyan"),lwd=8,cex=0.7,ncol=1)
+legend("bottomleft",c("fraction seqs.","sum bits", "weighted sum bits", "single-sequence sum bits"),col=c("red","blue","purple", "cyan"),lwd=8,cex=0.7,ncol=1)
 
 plot(rocFS$ACC, rocFS$FDR, col="red", lwd=8, type="l",ylim=c(0.2,1),xlim=c(0.2,1),xlab="ACC",ylab="FDR",main="ROC-like plot")      
 lines(rocSB$ACC, rocSB$FDR, col="blue",lwd=8)
@@ -32,7 +37,6 @@ points(rocFS$ACC[rocFS$MCC==max(rocFS$MCC)], rocFS$FDR[rocFS$MCC==max(rocFS$MCC)
 points(rocSB$ACC[rocSB$MCC==max(rocSB$MCC)], rocSB$FDR[rocSB$MCC==max(rocSB$MCC)], col="blue",pch="x",lwd=12)
 points(rocWB$ACC[rocWB$MCC==max(rocWB$MCC)], rocWB$FDR[rocWB$MCC==max(rocWB$MCC)], col="purple",pch="x",lwd=12)
 points(rocSSSB$ACC[rocSSSB$MCC==max(rocSSSB$MCC)], rocSSSB$FDR[rocSSSB$MCC==max(rocSSSB$MCC)], col="cyan",pch="x",lwd=12)
-legend("bottomleft",c("fraction seqs.","sum bits", "weighted sum bits", "single-sequence sum bits"),col=c("red","blue","purple", "cyan"),lwd=8,cex=0.7,ncol=1)
 
 plot(rocFS$fracSeqs/max(rocFS$fracSeqs), rocFS$MCC, col="red", lwd=8, type="l",ylim=c(0,max(rocSB$MCC)),xlim=c(0,1),xlab="threshold",ylab="MCC",main="MCC vs threshold")      
 lines(rocSB$sumBits/max(rocSB$sumBits), rocSB$MCC, col="blue",lwd=8)
@@ -49,34 +53,41 @@ dev.off()
 
 ########
 
-pdf(file="ROC.pdf", width=20, height=20)
+pdf(file="figures/ROC.pdf", width=20, height=20)
 op<-par(mfrow=c(1,1),cex=5.0)
 plot(rocSB$SPEC, rocSB$SENS, col="blue", lwd=12, type="l",ylim=c(0,1),xlim=c(0,1),xlab="Specificity",ylab="Sensitivity",main="ROC plot")      
 #lines(rocSB$SPEC, rocSB$SENS, col="blue",lwd=12)
 #lines(rocWB$SPEC, rocWB$SENS, col="purple",lwd=12)
 lines(rocSSSB$SPEC, rocSSSB$SENS, col="red",lwd=8)
+lines(rocB2ALL$SPEC, rocB2ALL$SENS, col="rosybrown",lwd=8)
 #points(rocFS$SPEC[rocFS$MCC==max(rocFS$MCC)], rocFS$SENS[rocFS$MCC==max(rocFS$MCC)], col="red",   pch="x",lwd=16)
 points(rocSB$SPEC[rocSB$MCC==max(rocSB$MCC)], rocSB$SENS[rocSB$MCC==max(rocSB$MCC)], col="blue",  pch="x",lwd=16)
 #points(rocWB$SPEC[rocWB$MCC==max(rocWB$MCC)], rocWB$SENS[rocWB$MCC==max(rocWB$MCC)], col="purple",pch="x",lwd=16)
 points(rocSSSB$SPEC[rocSSSB$MCC==max(rocSSSB$MCC)], rocSSSB$SENS[rocSSSB$MCC==max(rocSSSB$MCC)], col="red",pch="x",lwd=12)
-legend("bottomleft",c("alignment sum-bits", "sequence sum bits"),col=c("blue","red"),lwd=12,cex=0.7,ncol=1)
+points(rocB2ALL$SPEC[rocB2ALL$MCC==max(rocB2ALL$MCC)], rocB2ALL$SENS[rocB2ALL$MCC==max(rocB2ALL$MCC)], col="rosybrown",pch="x",lwd=12)
+lines(c(0.93,0.6),c(0.76,0.6), col="blue",lwd=4)
+text(0.6,0.6,'sum-bits=145\nmaxMCC=0.68',pos=2,col="blue",cex=0.5)
+lines(c(1,0.8),c(0.48,0.4), col="rosybrown",lwd=4)
+text(0.8,0.4,'bits=18.5\nmaxMCC=0.66',pos=2,col="rosybrown",cex=0.5)
+text(0.99,0.22,'bits=19\nmaxMCC=0.38',pos=2,col="red",cex=0.5)
+legend("bottomleft",c("Rfam alignments (sum-bits)", "Rfam sequences (bits)", "RMfam sequences (bits)"),col=c("blue","red","rosybrown"),lwd=12,cex=0.7,ncol=1)
 dev.off()
 
 ########
 
-pdf(file="ROC_thresh.pdf", width=20, height=20)
+pdf(file="figures/ROC_thresh.pdf", width=20, height=20)
 op<-par(mfrow=c(1,1),cex=5.0)
 #plot(rocSB$sumBits/max(rocSB$sumBits), rocSB$MCC,                 col="blue",lwd=12, type="l",ylim=c(0,max(rocSB$MCC)),xlim=c(0,1),xlab="threshold",ylab="MCC",main="MCC vs threshold")      
 plot(rocSB$sumBits, rocSB$MCC,                 col="blue",lwd=12, type="l",ylim=c(0,max(rocSB$MCC)),xlim=c(0,1000),xlab="threshold (bits)",ylab="MCC",main="MCC vs threshold")      
 #lines(rocSB$sumBits/max(rocSB$sumBits), rocSB$MCC,                 col="blue",lwd=12)
 #lines(rocWB$weightedSumBits/max(rocWB$weightedSumBits), rocWB$MCC, col="purple",lwd=12)
 #lines(rocSSSB$sngl_seq_sumBits/max(rocSSSB$sngl_seq_sumBits), rocSSSB$MCC, col="red",lwd=8)
-lines(rocSSSB$sngl_seq_sumBits, rocSSSB$MCC, col="red",lwd=8)
-legend("topright",c("alignment sum bits", "sequence sum bits"),col=c("blue","red"),lwd=12,cex=0.7,ncol=1)
+lines(rocSSSB$sngl_seq_sumBits, rocSSSB$MCC, col="red",lwd=12)
+legend("bottomright",c("Rfam alignment (sum bits)", "Rfam sequences (bits)"),col=c("blue","red"),lwd=12,cex=0.7,ncol=1)
 dev.off()
 
 ######################################################################
-#benchmark2:
+#benchmark2: individual motifs
 #cat ROC_benchmark2.dat | cut -f 13 | sort -d | uniq | awk '{print "\42"$1"\42,"}'
 motif<-c(
 "ANYA",
@@ -114,14 +125,118 @@ motif<-c(
 "vapC_target",
 "VTS1_binding"
 );
-rocB2      <-read.table("ROC_benchmark2.dat",          header = T, sep = "\t")
+rocB2      <-read.table("ROC_benchmark2.dat",              header = T, sep = "\t")
+rocB2A     <-read.table("ROC_data_sumBits_benchmark2.dat", header = T, sep = "\t")
+thresholds <-read.table("thresholds.dat",                  header = T, sep = "\t")
 
-pdf(file="ROC_benchmark2.pdf", width=20, height=20)
+pdf(file="figures/ROC_benchmark2.pdf", width=20, height=20)
 op<-par(mfrow=c(1,1),cex=5.0)
-plot(c(1,0), c(0,1), col="black",lwd=12, type="l",ylim=c(0,1),xlim=c(0,1),xlab="Specificity",ylab="Sensitivity",main="ROC curve: benchmark2")      
+plot(c(1,0), c(0,1), col="black",lwd=12, type="l",ylim=c(0,1),xlim=c(0,1),xlab="Specificity",ylab="Sensitivity",main="ROC curve: RMfam CMs to seqs")      
 for(i in 1:length(motif)){
       lines(rocB2$SPEC[rocB2$motif==motif[i]], rocB2$SENS[rocB2$motif==motif[i]], lty=i, col=i, lwd=12)
 }
 legend("bottomleft", motif, col=1:length(motif), lty=1:length(motif), cex=0.3, ncol=3)
 dev.off()
 
+
+
+emptyColnames<-motif
+maxMCC<-mat.or.vec(3,length(motif))
+sumMaxMCC<-mat.or.vec(1,length(motif))
+for(i in 1:length(motif)){
+
+      maxMCC[1,i]<-max(rocB2$MCC[rocB2$motif==motif[i]])
+
+      if(max(rocSSSBM$MCC[rocSSSBM$motif==motif[i]])>0){
+	maxMCC[2,i]<-max(rocSSSBM$MCC[rocSSSBM$motif==motif[i]])
+      }
+      else{
+	maxMCC[2,i]<-0
+      }
+
+      if(max(rocB2A$MCC[rocB2A$motif==motif[i]])>0){
+	maxMCC[3,i]<-max(rocB2A$MCC[rocB2A$motif==motif[i]])
+      }
+      else{
+	maxMCC[3,i]<-0
+      }
+      
+      sumMaxMCC[i]<-maxMCC[1,i]+maxMCC[2,i]+maxMCC[3,i]
+      emptyColnames[i]<-"";
+}
+maxMCCs<-sort(sumMaxMCC,decreasing=T,index.return=T)
+
+maxMCC<-array(data = maxMCC[,maxMCCs$ix], dim = c(3,length(motif)))
+colnames(maxMCC)<-motif[maxMCCs$ix]	  
+
+pdf(file="figures/maxMCC_benchmark2.pdf", width=20, height=30)
+op<-par(mfrow=c(1,1),cex=5.0,las=1,mar=c(5, 4+2, 4, 2+1)+0.1)#bottom, left, top, right
+barplot(maxMCC, ylab="", xlim=c(0,3), xlab="max(MCC)", main="MCC values for RMfam motifs",col=c("rosybrown","red","blue"),horiz = TRUE, cex.names=0.75 )#,names.arg=emptyColnames) cex.names=1.4, cex.axis = 1.6, 
+legend("topright",c("Rfam alignments (sum-bits)", "Rfam sequences (bits)", "RMfam sequences (bits)"),fill=c("blue","red","rosybrown"),ncol=1,cex=0.6) #,cex=1.2
+dev.off()
+
+
+
+for(i in 1:length(motif)){
+
+pdfName<-sprintf("figures/%s_benchmark.pdf",   motif[i])
+pdf(file=pdfName, width=20, height=20)
+op<-par(mfrow=c(2,2),cex=3.0)
+
+main<-sprintf("ROC curve: %s",   motif[i])
+plot(c(1,0), c(0,1), col="black",lwd=6, type="l",ylim=c(0,1),xlim=c(0,1),xlab="Specificity",ylab="Sensitivity",main=main)
+lines(rocB2$SPEC[rocB2$motif==motif[i]], rocB2$SENS[rocB2$motif==motif[i]], lty=1, col="rosybrown", lwd=6)
+lines(rocB2A$SPEC[rocB2A$motif==motif[i]], rocB2A$SENS[rocB2A$motif==motif[i]], lty=1, col="blue", lwd=6)
+lines(rocSSSBM$SPEC[rocSSSBM$motif==motif[i]], rocSSSBM$SENS[rocSSSBM$motif==motif[i]], lty=1, col="red", lwd=6)
+legend("bottomleft",c("Rfam alignments (sum-bits)", "Rfam sequences (bits)", "RMfam sequences (bits)"),col=c("blue","red","rosybrown"),lwd=12,cex=0.7,ncol=1)
+
+main<-sprintf("ROC-like curve: %s",   motif[i])
+plot(c(1,0), c(0,1), col="black",lwd=6, type="l",ylim=c(0,1),xlim=c(0,1),xlab="PPV",ylab="Sensitivity",main=main)
+lines(rocB2$PPV[rocB2$motif==motif[i]], rocB2$SENS[rocB2$motif==motif[i]], lty=1, col="rosybrown", lwd=6)
+lines(rocB2A$PPV[rocB2A$motif==motif[i]], rocB2A$SENS[rocB2A$motif==motif[i]], lty=1, col="blue", lwd=6)
+lines(rocSSSBM$PPV[rocSSSBM$motif==motif[i]], rocSSSBM$SENS[rocSSSBM$motif==motif[i]], lty=1, col="red", lwd=6)
+legend("bottomleft",c("Rfam alignments (sum-bits)", "Rfam sequences (bits)", "RMfam sequences (bits)"),col=c("blue","red","rosybrown"),lwd=12,cex=0.7,ncol=1)
+
+
+###
+thresh<-thresholds$GA[thresholds$NAME==motif[i]]
+main<-sprintf("Score vs MCC: %s",   motif[i])
+op<-par(las=2)
+plot(rocB2$bits[rocB2$motif==motif[i]], rocB2$MCC[rocB2$motif==motif[i]], lty=1, col="rosybrown", lwd=6, type="l",ylim=c(-1,1),xlim=c(0,50),xlab="CM score (bits)",ylab="MCC",main=main)
+lines(rocB2A$bits[rocB2A$motif==motif[i]], rocB2A$MCC[rocB2A$motif==motif[i]], lty=1, col="blue", lwd=6, type="l")
+lines(rocSSSBM$sngl_seq_sumBits[rocSSSBM$motif==motif[i]], rocSSSBM$MCC[rocSSSBM$motif==motif[i]], lty=1, col="red", lwd=6, type="l")
+lines(c(0,1000), c(0,0), lty=1, col="black",)
+lines(c(thresh,thresh), c(-1,1), lty=1, col="black")
+
+###
+maxMCCme<-rocB2[rocB2$MCC==maxMCC[1,colnames(maxMCC)==motif[i]] & rocB2$motif==motif[i],]
+maxMCCme<-maxMCCme[maxMCCme$TN==max(maxMCCme$TN),]
+maxMCCme<-maxMCCme[maxMCCme$FN==min(maxMCCme$FN),6:12]
+
+maxMCCSme<-rocSSSBM[rocSSSBM$MCC==maxMCC[2,colnames(maxMCC)==motif[i]] & rocSSSBM$motif==motif[i], ]
+if(length(row.names(maxMCCSme))==0){
+	maxMCCSme<-as.data.frame(array(data = 0*1:13, dim = c(1,13)))
+	colnames(maxMCCSme)<-colnames(rocSSSBM)
+}
+maxMCCSme<-maxMCCSme[maxMCCSme$TN==max(maxMCCSme$TN),]
+maxMCCSme<-maxMCCSme[maxMCCSme$FN==min(maxMCCSme$FN),6:12]
+
+maxMCCAme<-rocB2A[rocB2A$MCC==maxMCC[3,colnames(maxMCC)==motif[i]] & rocB2A$motif==motif[i], ]
+if(length(row.names(maxMCCAme))==0){
+	maxMCCAme<-as.data.frame(array(data = 0*1:13, dim = c(1,13)))
+	colnames(maxMCCAme)<-colnames(rocB2A)
+}
+maxMCCAme<-maxMCCAme[maxMCCAme$TN==max(maxMCCAme$TN),]
+maxMCCAme<-maxMCCAme[maxMCCAme$FN==min(maxMCCAme$FN),6:12]
+
+stats<-array(data = as.matrix(c(maxMCCme, maxMCCSme, maxMCCAme)), dim = c(7,3)); 
+
+rownames(stats)<-colnames(rocB2[1,6:12])
+
+main<-sprintf("Performance: %s",   motif[i])
+op<-par(las=2)
+barplot(t(stats), xlab="", ylab="Stats", ylim=c(0,3), main=main,col=c("rosybrown","red","blue"))
+
+dev.off()
+
+}
